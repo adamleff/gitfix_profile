e0a556825df07f70a5e8ad15bbb5bd3ec0e29e38
Adam Leff <adam@leff.co>
2018-01-12 14:40:09 -0800

    master

---
+++ controls/example.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+# copyright: 2017, The Authors
+
+control 'on master' do
+  name 'on master'
+  desc 'on master'
+  impact 1.0
+  describe 1 do
+    it { should cmp 1 }
+  end
+end
+
