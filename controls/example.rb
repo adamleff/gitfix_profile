e623d8b0732f6958d8bd80ab9ce94886f2aaf48f
Adam Leff <adam@leff.co>
2018-01-12 14:40:42 -0800

    on branch1
    Signed-off-by: Adam Leff <adam@leff.co>

--- controls/example.rb
+++ controls/example.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 # copyright: 2017, The Authors
 
-control 'on master' do
-  name 'on master'
-  desc 'on master'
+control 'on branch1' do
+  name 'on branch1'
+  desc 'on branch1'
   impact 1.0
   describe 1 do
     it { should cmp 1 }
