45f2ad917402fd45ad513154ac82c74cbd61f37d
Adam Leff <adam@leff.co>
2018-01-12 16:14:36 -0800

    stff

--- controls/example.rb
+++ controls/example.rb
@@ -2,8 +2,8 @@
 # copyright: 2017, The Authors
 
 control 'on master' do
-  title 'on master'
-  desc 'on master'
+  title 'on v1'
+  desc 'on v1'
   impact 1.0
   describe 1 do
     it { should cmp 1 }
